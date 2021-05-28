1012693fb702a1839ce2d3e17c3e126f9f096f99
Andres Osorio <aosorios@psl.com.co>
2021-05-28 08:20:34 -0500

    add change to orders

--- orders/src/events/listeners/payment-created-listener.ts
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -16,7 +16,7 @@ export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
     const order = await Order.findById(data.orderId);
 
     if (!order) {
-      throw new Error('Order not found');
+      throw new Error('Order was not found');
     }
 
     order.set({
